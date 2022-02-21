92b08482f4fb022a3f8686e6943676b4ee46c88e
VeronicaSotoF <sotofierroveronicadev@gmail.com>
2022-02-21 13:42:43 -0600

    full keys

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
-let firstNumber = document.querySelector('#firstNumber').addEventListener('input', function (e) {
+let firstInput = document.querySelector('#firstNumber')
+
+let secondInput = document.querySelector('#secondNumber')
+
+let firstNumber = firstInput.addEventListener('input', function (e) {
     firstNumber = parseFloat(e.target.value)
 })
 
-let secondNumber = document.querySelector('#secondNumber').addEventListener('input', function (e) {
+let secondNumber = secondInput.addEventListener('input', function (e) {
     secondNumber = parseFloat(e.target.value)
 })
 
@@ -56,3 +60,54 @@ document.querySelector('#clear-btn').addEventListener('click', function (e) {
     document.querySelector('#secondNumber').value = secondNumber;
 })
 
+
+function fillInputValue (value) {
+    if (firstInput.value === '') {
+        firstInput.value = value;
+        firstNumber = value;
+    }
+    else {
+        secondInput.value = value;
+        secondNumber = value;
+    }
+}
+
+document.querySelector('#zero-btn').addEventListener('click', function (e) {
+    fillInputValue(0);
+})
+
+document.querySelector('#one-btn').addEventListener('click', function (e) {
+    fillInputValue(1);
+})
+
+document.querySelector('#two-btn').addEventListener('click', function (e) {
+    fillInputValue(2);
+})
+
+document.querySelector('#three-btn').addEventListener('click', function (e) {
+    fillInputValue(3);
+})
+
+document.querySelector('#four-btn').addEventListener('click', function (e) {
+    fillInputValue(4);
+})
+
+document.querySelector('#five-btn').addEventListener('click', function (e) {
+    fillInputValue(5);
+})
+
+document.querySelector('#six-btn').addEventListener('click', function (e) {
+    fillInputValue(6);
+})
+
+document.querySelector('#seven-btn').addEventListener('click', function (e) {
+    fillInputValue(7);
+})
+
+document.querySelector('#eight-btn').addEventListener('click', function (e) {
+    fillInputValue(8);
+})
+
+document.querySelector('#nine-btn').addEventListener('click', function (e) {
+    fillInputValue(9);
+})
